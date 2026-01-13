9a3c54c935f83a88f12ef45940757114edf1a123
Lucas Anacleto <lucasanacleto04@gmail.com>
2026-01-13 17:54:38 -0300

    Initial Commit

---
+++ CodeBreaker/CodeBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerApp.swift
+//  CodeBreaker
+//
+//  Created by Lucas Anacleto on 13/01/26.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
