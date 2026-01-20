2144d7560c557a851f48dde024bbe0b974151fd5
Lucas Anacleto <lucasanacleto04@gmail.com>
2026-01-19 23:04:03 -0300

    Iniciando desenvolvimento do CodeBreaker e entendendo como montar o visual e a lógica por trás do jogo

--- CodeBreaker/ContentView.swift
+++ CodeBreaker/ContentView.swift
@@ -10,14 +10,25 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            pegs(colors: [.red, .green, .green, .blue])
+            pegs(colors: [.green, .green, .green, .blue])
+            pegs(colors: [.blue, .red, .red, .blue])
         }
         .padding()
     }
+    
+    func pegs(colors: Array<Color>) -> some View {
+        HStack{
+            ForEach(colors.indices, id: \.self) { index in
+                RoundedRectangle(cornerRadius: 10).foregroundStyle(colors[index])
+                    .aspectRatio(1, contentMode: .fit)
+            }
+            MatchMarkers(matches: [.exact,.inexact,.nomatch,.exact])
+        }
+    }
 }
+
+
 
 #Preview {
     ContentView()
---
+++ CodeBreaker/MatchMarkers.swift
@@ -0,0 +1,44 @@
+//
+//  MatchMarkers.swift
+//  CodeBreaker
+//
+//  Created by Lucas Anacleto on 19/01/26.
+//
+
+import SwiftUI
+
+enum Match {
+    case nomatch
+    case exact
+    case inexact
+}
+
+struct MatchMarkers: View {
+    var matches: [Match]
+    
+    var body: some View {
+        VStack {
+            HStack {
+                matchMarker(peg: 0)
+                matchMarker(peg: 1)
+            }
+            HStack {
+                matchMarker(peg: 2)
+                matchMarker(peg: 3)
+            }
+        }
+    }
+    
+    func matchMarker(peg: Int) -> some View {
+        let exactCount: Int = matches.count(where: {match in match == .exact}) 
+        let foundCount: Int = matches.count(where: {match in match != .nomatch})
+        
+        return Circle()
+            .fill(exactCount > peg ? Color.primary : Color.clear)
+            .strokeBorder(foundCount > peg ? Color.primary : Color.clear, lineWidth: 2).aspectRatio(1, contentMode: .fit)
+    }
+}
+
+#Preview {
+    MatchMarkers(matches: [.exact,.exact,.inexact])
+}
